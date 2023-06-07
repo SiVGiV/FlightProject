2ead483f1ec3bbe9a9b5f98e7af9c98bfcbdab91
SiVGiV <sivan95510@gmail.com>
2023-06-07 13:01:06 +0300

    Updated paths for setup data and added userroles

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 python setup_db.py create_schema
 python manage.py migrate
-python manage.py loaddata countries.json
+python manage.py loaddata static/setupdata/countries.json
+python manage.py loaddata static/setupdata/userroles.json
