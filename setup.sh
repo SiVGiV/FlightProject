10d48cca2b86025b6d70e1f484ecb70b01a53679
SiVGiV <sivan95510@gmail.com>
2023-06-08 14:31:45 +0300

    Updated setup script and fixtures

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 python setup_db.py create_schema
 python manage.py migrate
-python manage.py loaddata static/setupdata/countries.json
-python manage.py loaddata static/setupdata/userroles.json
+python manage.py loaddata FlightsApi/fixtures/countries.json
