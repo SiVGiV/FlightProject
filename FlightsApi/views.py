5c935fc1b20cbacf3a1e044dc353cf03e2de65d1
SiVGiV <sivan95510@gmail.com>
2023-06-08 15:14:21 +0300

    Created a basic registration view for testing data creation - NOT FOR PRODUCTION

--- FlightsApi/views.py
+++ FlightsApi/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+class RegisterView(CreateView):
+    template_name = "register.html"
+    form_class = UserRegisterForm
