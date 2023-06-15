d1cd2baa55328409545a84980a39853f5d0626b4
sivan <sivan95510@gmail.com>
2023-06-15 12:34:44 +0300

    Created test for @accepts, Imported unittest.TestCase for non django tests

--- FlightsApi/tests/test_repository.py
+++ FlightsApi/tests/test_repository.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
+from unittest import TestCase as BasicTestCase
+
 from ..repository.repository import Repository
 from ..repository.typechecking import verify_model, accepts
 from ..repository.errors import FetchError, CreationError, UpdateError, WrongModelType
 from ..models import User, Admin, AirlineCompany, Customer, Country, Flight, Ticket
 
+
 from django.utils import timezone
 from datetime import timedelta, date
 
 
-class TestVerifyModel(TestCase):
+class TestVerifyModel(BasicTestCase):
     def test_model_class(self):
         test_func = verify_model(lambda x: True)
         self.assertTrue(test_func(User))
@@ -21,6 +24,49 @@ class TestVerifyModel(TestCase):
         test_func = verify_model(lambda x: True)
         self.assertRaises(WrongModelType, lambda: test_func(1))
 
+
+class TestAccepts(BasicTestCase):
+    @staticmethod
+    @accepts(int, str, bool)
+    def dummy_func(integer, string, boolean):
+        if  (type(integer) is not int) or \
+            (type(string) is not str) or \
+            (type(boolean) is not bool):
+            return "Failure"
+        else:
+            return "Success"
+                    
+    def test_accepts_empty_variables(self):
+        @accepts()
+        def local_dummy():
+            return "Success"
+        self.assertEqual("Success", local_dummy())
+                    
+    def test_accepts_correct_types(self):
+        self.assertEqual("Success", self.dummy_func(1, "1", True))
+    
+    def test_accepts_blocked_variables(self):
+        with self.subTest("Bad int"):
+            self.assertRaises(TypeError, lambda: self.dummy_func("err", "1", True))
+        with self.subTest("Bad string"):
+            self.assertRaises(TypeError, lambda: self.dummy_func(1, 1, True))
+        with self.subTest("Bad bool"):
+            self.assertRaises(TypeError, lambda: self.dummy_func(1, "1", "err"))
+            
+    def test_accepts_kwarg_mix(self):
+        with self.subTest("Half kwargs"):
+            self.assertEqual("Success", self.dummy_func(1, "1", boolean=True))
+            self.assertRaises(TypeError, lambda: self.dummy_func("err", "1", boolean=True))
+            self.assertRaises(TypeError, lambda: self.dummy_func(1, "1", boolean="err"))
+        
+        with self.subTest("Only kwargs"):
+            self.assertEqual("Success", self.dummy_func(integer=1, string="1", boolean=True))
+            self.assertEqual("Success", self.dummy_func(integer=1, boolean=True, string="1")) # Shuffled
+            self.assertRaises(TypeError, lambda: self.dummy_func(integer="err", string="1", boolean=True))
+            self.assertRaises(TypeError, lambda: self.dummy_func(integer=1, string="1", boolean="err"))
+            self.assertRaises(TypeError, lambda: self.dummy_func(string="1", integer=1, boolean="err")) # Shuffled
+            
+            
 
 class TestGetById(TestCase):
     def test_get_by_id_success(self):
