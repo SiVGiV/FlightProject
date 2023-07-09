de141ae39ef9f2c5cf7976b3367567562d187749
SiVGiV <sivan95510@gmail.com>
2023-07-09 12:35:37 +0300

    fixed bug

--- FlightsApi/views/__init__.py
+++ FlightsApi/views/__init__.py
@@ -1,6 +1,6 @@
 from .admin_views import AdminView, AdminsView
 from .airline_views import AirlineView, AirlinesView
-from .customer_views import CustomerView, CustomersView, update_customer_view
+from .customer_views import CustomerView, CustomersView
 from .flight_views import FlightView, FlightsView
 from .ticket_views import TicketView, TicketsView
 from .country_views import CountryView, CountriesView
--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ python manage.py loaddata countries
 :: DJANGO_SUPERUSER_USERNAME
 :: DJANGO_SUPERUSER_PASSWORD
 :: DJANGO_SUPERUSER_EMAIL
-set DJANGO_SUPERUSER_USERNAME="admin"
-set DJANGO_SUPERUSER_PASSWORD="admin"
-set DJANGO_SUPERUSER_EMAIL="a@a.com"
+set DJANGO_SUPERUSER_USERNAME=admin
+set DJANGO_SUPERUSER_PASSWORD=admin
+set DJANGO_SUPERUSER_EMAIL=a@a.com
 python manage.py createsuperuser --noinput
 python setup_db.py make_superuser_admin %DJANGO_SUPERUSER_USERNAME%
