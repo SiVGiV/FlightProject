b43498a69f03806fdcd96bb777a5047cc26c876b
SiVGiV <sivan95510@gmail.com>
2023-07-17 00:12:05 +0300

    Fixed static files

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     python setup_db.py
     python manage.py migrate
     python manage.py loaddata countries
+    python manage.py collectstatic
     python manage.py createsuperuser --noinput
     python setup_db.py make_superuser_admin $DJANGO_SUPERUSER_USERNAME
 fi
 # This will run every time
-gunicorn FlightProject.wsgi:application
+gunicorn FlightProject.wsgi:application --bind 0.0.0.0:8000
