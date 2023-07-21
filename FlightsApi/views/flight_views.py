9b5450910ecc6c9f1155879998631476fc4cb7ab
SiVGiV <sivan95510@gmail.com>
2023-07-21 16:22:44 +0300

    same bug

--- FlightsApi/views/flight_views.py
+++ FlightsApi/views/flight_views.py
@@ -152,8 +152,12 @@ class FlightView(APIView): # /flight/<id>
             arrival_datetime_str = request.data.get('arrival_datetime')
             if departure_datetime_str:
                 departure_datetime = parser.parse(departure_datetime_str)
+            else:
+                departure_datetime = None
             if arrival_datetime_str:
                 arrival_datetime = parser.parse(arrival_datetime_str)
+            else:
+                arrival_datetime = None
         except ValueError as e:
             logger.info(e)
             code, data = bad_request_response("'date' must be in the ISO 8601 format.")
