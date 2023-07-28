3adb9b7dd69d879b5f6eb1946a241b546cd48175
SiVGiV <sivan95510@gmail.com>
2023-07-28 12:30:23 +0300

    Disabled APPEND_SLASH to prevent API mistakes

--- backend/FlightProject/settings.py
+++ backend/FlightProject/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FlightProject.wsgi.application'
 
+APPEND_SLASH = False
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -110,8 +111,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = "FlightsApi.User" 
 
+AUTH_USER_MODEL = "FlightsApi.User" 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
